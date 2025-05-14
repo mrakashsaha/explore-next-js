a3c8c91ba3c9d50f5c6124b9c0112dbf065d31c2
Akash Kumar Saha <mr.akashsaha@gmail.com>
2025-05-14 09:16:02 +0600

    Initial setup of Next.js Project

---
+++ app/page.js
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="mt-10">
+        <h2>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur, sint ipsum? Reprehenderit odit dolorem repellendus aperiam. Doloremque voluptates fugit beatae temporibus non laboriosam cumque aliquam debitis, itaque porro est saepe.
+        In ullam repellat blanditiis asperiores esse, similique natus modi odio saepe ipsa exercitationem sed maiores ad assumenda pariatur, soluta illo. Exercitationem inventore corrupti similique ipsum consectetur deserunt, necessitatibus quod sapiente?
+        Ea, maxime eum? Ipsum possimus rerum, ea a laboriosam asperiores consequatur iure provident repellendus aperiam voluptatibus velit eius repellat doloremque? Facilis accusantium ab veniam consequatur sunt quos id illo dolorem.
+        Totam hic nostrum, porro animi itaque maiores repudiandae possimus, minima unde nisi sit autem dolore minus magni atque optio exercitationem sunt doloremque fugiat ratione adipisci. Excepturi facere quod deleniti voluptatum.
+        Maiores totam dolor in rerum, eaque harum illo nemo aperiam molestiae maxime quibusdam recusandae voluptatibus sapiente repudiandae facilis perferendis minus! Voluptates velit error vitae veniam tempora possimus aspernatur sequi at?
+        Accusamus ex odio, nulla, corporis nihil ab quo amet assumenda nemo quis sit beatae laboriosam, quidem eius inventore commodi placeat a veritatis veniam omnis fugit reiciendis dicta. Magni, consectetur consequuntur?
+        Suscipit perferendis totam recusandae laborum aperiam nam optio qui dicta quia odit, doloremque maiores perspiciatis sed ducimus rerum aut animi nihil quas labore hic! Odio atque perspiciatis necessitatibus illum adipisci.
+        Doloribus qui ad itaque nemo neque consectetur laboriosam beatae sint debitis. Nisi, commodi! Aspernatur odio, reiciendis quod cumque, consequuntur voluptate officia ducimus a provident nulla molestias! Perspiciatis omnis molestiae pariatur?
+        Accusamus at suscipit itaque ipsam alias? Repellat esse blanditiis quia est fuga possimus porro vel ipsam? Autem excepturi minus maiores at magnam reprehenderit, illo alias earum quidem nemo? Error, sequi!
+        Vitae accusamus excepturi cumque non reiciendis corrupti magni quisquam eligendi officiis dolor quam inventore, accusantium atque nihil fugiat optio dicta nemo commodi eius quod vel! Necessitatibus non modi maiores nisi.</h2>
+    </main>
+  );
+}
